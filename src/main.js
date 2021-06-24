c260a5f886ae416cce0c84ee028e6ce0c5120823
Shiva Poudel <dev.shivapoudel@gmail.com>
2021-06-25 01:07:46 +0545

    Refactored code and introduced support for active icon plan implementation

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import iconMap from './iconMap';
 import iconsCache from './iconsCache';
 
 // Expected configuration.
+iconMap.options = {
+  ...iconMap.options,
+  ...{
+    activeIconPack: 'react_redux',
+  },
+};
+
 iconMap.fileExtensions = {
   ...iconMap.fileExtensions,
   ...{
@@ -42,17 +49,28 @@ function replaceIcon(fileRow, iconMap) {
     fileRow.querySelector('[role=rowheader]')?.firstElementChild?.firstElementChild?.innerText;
   if (!fileName) return; // fileName couldn't be found or we don't have a match for it.
 
+  // Get file extension.
+  const fileExtension = fileName.match(/.*?[.](?<ext>xml.dist|yml.dist|\w+)$/)?.[1];
+
   // SVG to be replaced.
   const svgEl = fileRow.querySelector('.octicon');
   if (!svgEl) return; // couldn't find svg element.
 
   // Get Directory or File type.
   const isDir = svgEl.getAttribute('aria-label') === 'Directory';
+  const lowerFileName = fileName.toLowerCase();
 
-  // Get icon filename.
-  let iconName = lookForMatch(fileName, isDir, iconMap); // returns iconname if found or undefined.
-  if (isLightTheme && iconMap.light.fileNames[fileName])
-    iconName = iconMap.light.fileNames[fileName];
+  // Get icon name.
+  let iconName = lookForMatch(fileName, lowerFileName, fileExtension, isDir, iconMap); // returns icon name if found or undefined.
+  if (isLightTheme) {
+    iconName = lookForLightMatch(iconName, fileName, fileExtension, isDir, iconMap); // returns icon name if found for light mode or undefined.
+  }
+
+  // Get folder icon from active icon pack.
+  if (iconMap.options.activeIconPack && isDir) {
+    iconName = lookForIconPackMatch(iconName, lowerFileName, iconMap);
+  }
+
   if (!iconName) return;
 
   const { innerHtml, viewBox } = iconsCache[iconName + '.svg'];
@@ -70,14 +88,13 @@ function replaceIcon(fileRow, iconMap) {
  * @since 1.0.0
  *
  * @param {String} fileName File name.
+ * @param {String} lowerFileName Lowercase file name.
+ * @param {String} fileExtension File extension.
  * @param {Boolean} isDir Check if directory or file type.
  * @param {Object} iconMap Icon map.
  * @returns {String} The matched icon name.
  */
-function lookForMatch(fileName, isDir, iconMap) {
-  const lowerFileName = fileName.toLowerCase();
-  const fileExtension = fileName.match(/.*?[.](?<ext>xml.dist|yml.dist|\w+)$/)?.[1];
-
+function lookForMatch(fileName, lowerFileName, fileExtension, isDir, iconMap) {
   // First look in fileNames and folderNames.
   if (iconMap.fileNames[fileName] && !isDir) return iconMap.fileNames[fileName];
   if (iconMap.folderNames[fileName] && isDir) return iconMap.folderNames[fileName];
@@ -93,4 +110,69 @@ function lookForMatch(fileName, isDir, iconMap) {
   // Fallback into default file or folder if no matches.
   if (!isDir) return 'file';
   if (isDir) return 'folder';
+}
+
+/**
+ * Lookup for matched light file/folder icon name.
+ *
+ * @since 1.4.0
+ *
+ * @param {String} iconName Icon name.
+ * @param {String} fileName File name.
+ * @param {String} fileExtension File extension.
+ * @param {Boolean} isDir Check if directory or file type.
+ * @param {Object} iconMap Icon map.
+ * @returns {String} The matched icon name.
+ */
+function lookForLightMatch(iconName, fileName, fileExtension, isDir, iconMap) {
+  // First look in fileNames and folderNames.
+  if (iconMap.light.fileNames[fileName] && !isDir) return iconMap.light.fileNames[fileName];
+  if (iconMap.light.folderNames[fileName] && isDir) return iconMap.light.folderNames[fileName];
+
+  // Look for extension in fileExtensions and languageIds.
+  if (iconMap.light.fileExtensions[fileExtension] && !isDir)
+    return iconMap.light.fileExtensions[fileExtension];
+
+  return iconName;
+}
+
+/**
+ * Lookup for matched folder icon from active icon pack.
+ *
+ * @since 1.4.0
+ *
+ * @param {String} iconName Icon name.
+ * @param {String} lowerFileName Lowercase file name.
+ * @param {Object} iconMap Icon map.
+ * @returns {String} The matched icon name.
+ */
+function lookForIconPackMatch(iconName, lowerFileName, iconMap) {
+  if (iconMap.options.activeIconPack) {
+    switch (iconMap.options.activeIconPack) {
+      case 'angular':
+      case 'angular_ngrx':
+        if (iconsCache[`folder-react-${lowerFileName}.svg`]) return `folder-ngrx-${lowerFileName}`;
+        break;
+      case 'react':
+      case 'react_redux':
+        if (iconsCache[`folder-react-${lowerFileName}.svg`]) {
+          return `folder-react-${lowerFileName}`;
+        } else if (iconsCache[`folder-redux-${lowerFileName}.svg`]) {
+          return `folder-redux-${lowerFileName}`;
+        }
+        break;
+      case 'vue':
+      case 'vue_vuex':
+        if (iconsCache[`folder-vuex-${lowerFileName}.svg`]) {
+          return `folder-vuex-${lowerFileName}`;
+        } else if (iconsCache[`folder-vue-${lowerFileName}.svg`]) {
+          return `folder-vue-${lowerFileName}`;
+        } else if ('nuxt' === lowerFileName) {
+          return `folder-nuxt`;
+        }
+        break;
+    }
+  }
+
+  return iconName;
 }
