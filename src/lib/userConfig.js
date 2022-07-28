f559bdfcb4ccb650afaa299bd48da102e97bdc1c
claudiohbsantos <claudiohbsantos@gmail.com>
2022-07-27 23:50:08 -0400

    cross-browser storage usage

--- src/lib/userConfig.js
+++ src/lib/userConfig.js
@@ -1,20 +1,22 @@
 const hardDefaults = {
   iconPack: 'react',
   iconSize: 'md',
-  extEnabled: true
+  extEnabled: true,
 };
 
 export const getConfig = (config, domain = window.location.hostname, useDefault = true) =>
-  chrome.storage.sync
-    .get({
-      // get custom domain config (if not getting default).
-      [`${domain !== 'default' ? domain : 'SKIP'}:${config}`]: null,
-      // also get user default as fallback
-      [`default:${config}`]: hardDefaults[config],
-    })
-    .then(
-      ({ [`${domain}:${config}`]: value, [`default:${config}`]: fallback }) => value ?? (useDefault ? fallback : null)
+  new Promise((resolve) => {
+    chrome.storage.sync.get(
+      {
+        // get custom domain config (if not getting default).
+        [`${domain !== 'default' ? domain : 'SKIP'}:${config}`]: null,
+        // also get user default as fallback
+        [`default:${config}`]: hardDefaults[config],
+      },
+      ({ [`${domain}:${config}`]: value, [`default:${config}`]: fallback }) =>
+        resolve(value ?? (useDefault ? fallback : null))
     );
+  });
 
 export const setConfig = (config, value, domain = window.location.hostname) =>
   chrome.storage.sync.set({
@@ -22,9 +24,13 @@ export const setConfig = (config, value, domain = window.location.hostname) =>
   });
 
 export const clearConfig = (config, domain = window.location.hostname) =>
-  chrome.storage.sync.remove( `${domain}:${config}`);
+  new Promise((resolve) => {
+    chrome.storage.sync.remove(`${domain}:${config}`, resolve);
+  });
 
 export const onConfigChange = (config, handler, domain = window.location.hostname) =>
-  chrome.storage.onChanged.addListener((changes) => {
-    changes[`${domain}:${config}`]?.newValue !== undefined && handler(changes[`${domain}:${config}`]?.newValue);
-  });
+  chrome.storage.onChanged.addListener(
+    (changes) =>
+      changes[`${domain}:${config}`]?.newValue !== undefined &&
+      handler(changes[`${domain}:${config}`]?.newValue)
+  );
