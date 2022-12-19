126e8f0c75db6d394f7b1116294f700d6dda3a3e
Saif Alaqqad <saifo1999@gmail.com>
2022-12-19 20:48:58 +0300

    Fix tree folder icon selector

--- src/providers/github.js
+++ src/providers/github.js
@@ -4,7 +4,7 @@ const githubConfig = {
     row: '.js-navigation-container[role=grid] > .js-navigation-item, file-tree .ActionList-content, a.tree-browser-result, tr.react-directory-row, div.PRIVATE_TreeView-item-content',
     filename:
       'div[role="rowheader"] > span, .ActionList-item-label, a.tree-browser-result > marked-text, .react-directory-truncate, .PRIVATE_TreeView-item-content-text',
-    icon: '.octicon-file, .octicon-file-directory-fill, .octicon-file-submodule, a.tree-browser-result > svg.octicon.octicon-file, .react-directory-filename-column svg, .PRIVATE_TreeView-item-visual svg.octicon',
+    icon: '.octicon-file, .octicon-file-directory-fill, .octicon-file-submodule, a.tree-browser-result > svg.octicon.octicon-file, .react-directory-filename-column svg, .PRIVATE_TreeView-directory-icon > svg.octicon, .PRIVATE_TreeView-item-visual > svg.octicon',
   },
   getIsLightTheme: () => document.querySelector('html').getAttribute('data-color-mode') === 'light',
   getIsDirectory: ({ icon }) => icon.getAttribute('aria-label') === 'Directory' || icon.getAttribute('class')?.includes('directory'),
