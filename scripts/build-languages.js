866254f115bb9745db5592bfc2e490d827ab6619
meche-gh <114668551+meche-gh@users.noreply.github.com>
2022-12-22 21:43:24 +0000

    Rewrite language data script & remove overrides

--- scripts/build-languages.js
+++ scripts/build-languages.js
@@ -1,27 +1,20 @@
-const rimraf = require('rimraf');
 const path = require('path');
 const api = require('@octokit/core');
 const fs = require('fs-extra');
-const fr = require('follow-redirects');
-const glob = require('glob');
+const fetch = require('node-fetch');
 const stringify = require('json-stable-stringify');
-const overrideMap = require('./language-overrides.json');
 const iconMap = require('../src/icon-map.json');
 
 const vsDataPath = path.resolve(__dirname, '..', 'data');
 const srcPath = path.resolve(__dirname, '..', 'src');
 
-rimraf.sync(vsDataPath);
-fs.ensureDirSync(vsDataPath);
-rimraf.sync(path.resolve(srcPath, 'language-map.json'));
-
-const resultsPerPage = 100; // max 100
-
 let index = 0;
 let total;
-let items = [];
+const items = [];
+const contributions = [];
 const languages = [];
 
+const resultsPerPage = 100; // max 100
 const octokit = new api.Octokit();
 const query = {
   page: 0,
@@ -30,116 +23,116 @@ const query = {
 };
 const GITHUB_RATELIMIT = 6000;
 
-console.log('[1/7] Querying Github API for official VSC language contributions.');
-queryLanguageContributions()
+async function main() {
+  await fs.remove(vsDataPath);
+  await fs.ensureDir(vsDataPath);
+  await fs.remove(path.resolve(srcPath, 'language-map.json'));
 
-function queryLanguageContributions() {
-  try {
-    octokit.request('GET /search/code', query).then(
-      (res) => {
-        if (!res.data) throw new Error();
-        query.page = index;
-        index += 1;
-        total = total || res.data.total_count;
-        items = items.concat(res.data.items);
-        if (resultsPerPage * index >= total) {
-          console.log('[2/7] Fetching Microsoft language contributions from Github.');
-          index = 0;
-          total = items.length;
-          items.forEach(fetchLanguageContribution);
-        } else {
-          setTimeout(queryLanguageContributions, GITHUB_RATELIMIT);
-        }
-      },
-      (reason) => {
-        throw new Error(reason);
-      }
-    );
-  } catch (reason) {
-    throw new Error(reason);
+  console.log('[1/7] Querying Github API for official VSC language contributions.');
+  queryLanguageContributions()
+}
+
+main();
+
+async function queryLanguageContributions() {
+  const res = await octokit.request('GET /search/code', query);
+  if (!res.data) throw new Error();
+  query.page = index;
+  index += 1;
+  if (!total) total = res.data.total_count;
+  items.push(...res.data.items);
+  if (resultsPerPage * index >= total) {
+    console.log('[2/7] Fetching Microsoft language contributions from Github.');
+    index = 0;
+    total = items.length;
+    items.forEach(fetchLanguageContribution);
+  } else {
+    setTimeout(queryLanguageContributions, GITHUB_RATELIMIT);
   }
 }
 
-function fetchLanguageContribution(item) {
-  const urlPath = item.html_url.replace(/\/blob\//, '/raw/');
+async function fetchLanguageContribution(item) {
+  const rawUrl = item.html_url.replace('/blob/', '/raw/');
   const resPath = item.path.replace(/[^/]+$/, 'extension.json');
-  const extPath = path.join(item.repository.name, resPath);
-  const extDir = path.dirname(extPath);
-  if (/sample|template/.test(extDir)) {
+  const extPath = path.join(vsDataPath, resPath);
+  let extManifest;
+  try {
+    extManifest = await fetch(rawUrl);
+    extManifest = await extManifest.text();
+  } catch (reason) {
+    throw new Error(reason);
+  }
+  try {
+    await fs.ensureDir(path.dirname(extPath));
+    await fs.writeFile(extPath, extManifest, 'utf-8');
+  } catch (reason) {
+    throw new Error(`${reason} (${extPath})`);
+  }
+  items[index] = [extPath, extManifest];
+  index += 1;
+  if (index === total) {
+    console.log('[3/7] Loading VSC language contributions into Node.');
+    index = 0;
+    items.forEach(loadLanguageContribution);
+  }
+}
+
+function loadLanguageContribution([extPath, extManifest]) {
+  let data;
+  try {
+    data = JSON.parse(extManifest.replace(/#\w+_\w+#/g, '0'));
+  } catch (error) {
+    throw new Error(`${error} (${extPath})`);
+  }
+  if (!data.contributes || !data.contributes.languages) {
     total -= 1;
     return;
   }
-  try {
-    fs.ensureDir(path.join(vsDataPath, extDir)).then(() => {
-      const extFile = fs.createWriteStream(path.join(vsDataPath, extPath));
-      fr.https
-        .get(urlPath, (res) => {
-          res.pipe(extFile);
-          res.on('end', () => {
-            index += 1;
-            if (index === total) {
-              console.log('[3/7] Loading VSC language contributions into Node.');
-              glob(path.join(vsDataPath, '**', 'extension.json'), (err, matches) => {
-                index = 0;
-                total = matches.length;
-                matches.forEach(loadLanguageContribution);
-              });
-            }
-          });
-        })
-        .on('error', (err) => {
-          fs.unlink(extPath);
-          throw err;
-        });
-    });
-  } catch (reason) {
-    throw new Error(reason);
-  }
-}
-
-function loadLanguageContribution(filePath) {
-  let data;
-  try {
-    data = JSON.parse(fs.readFileSync(filePath, 'utf-8').replace(/#\w+_\w+#/g, '0'));
-  } catch (error) {
-    throw new Error(`${error} (${filePath})`);
-  }
-  data.contributes = data.contributes || {};
-  data.contributes.languages = data.contributes.languages || [];
-  languages.push(...data.contributes.languages);
+  contributions.push(...data.contributes.languages);
   index += 1;
   if (index === total) {
     console.log('[4/7] Processing language contributions for VSC File Icon API compatibility.');
     index = 0;
-    total = languages.length;
-    languages.forEach(processLanguageContribution);
+    total = contributions.length;
+    contributions.forEach(processLanguageContribution);
   }
 }
 
-function processLanguageContribution(language) {
-  const { aliases, configuration, firstLine, ...lang } = language;
-  lang.extensions = lang.extensions || [];
-  lang.filenames = lang.filenames || [];
-  if (lang.filenamePatterns) {
-    lang.filenamePatterns.forEach((ptn) => {
+function processLanguageContribution(contribution) {
+  const { id, filenamePatterns } = contribution;
+  let { extensions, filenames } = contribution;
+  extensions = extensions || [];
+  filenames = filenames || [];
+  if (filenamePatterns) {
+    filenamePatterns.forEach((ptn) => {
       if (/^\*\.[^*/?]+$/.test(ptn)) {
-        lang.extensions.push(ptn.substring(1));
+        extensions.push(ptn.substring(1));
       }
       if (/^[^*/?]+$/.test(ptn)) {
-        lang.filenames.push(ptn);
+        filenames.push(ptn);
       }
     });
-    delete lang.filenamePatterns;
   }
-  lang.extensions = lang.extensions
+  extensions = extensions
     .map((ext) => ext.charAt(0) === '.' ? ext.substring(1) : ext)
     .filter((ext) => !/\*|\/|\?/.test(ext));
-  lang.filenames = lang.filenames.filter((name) => !/\*|\/|\?/.test(name));
-  languages[index] = lang;
+  filenames = filenames.filter((name) => !/\*|\/|\?/.test(name));
+  if (!filenames.length && !extensions.length) {
+    total -= 1;
+    return;
+  }
+  const language = languages.find(lang => lang.id === id);
+  if (language) {
+    language.filenames.push(...filenames);
+    language.extensions.push(...extensions);
+  } else {
+    languages.push({ id, extensions, filenames });
+  }
   index += 1;
   if (index === total) {
     console.log('[5/7] Mapping language contributions into file icon configuration.');
     index = 0;
+    total = languages.length;
     languages.forEach(mapLanguageContribution);
   }
 }
@@ -149,10 +142,10 @@ languageMap.fileExtensions = {};
 languageMap.fileNames = {};
 
 function mapLanguageContribution(lang) {
+  const langIcon = iconMap.languageIds[lang.id];
   lang.extensions.forEach((ext) => {
-    const iconName = handleIconRemapping(lang.id, 'extensions', 'fileExtensions', ext);
+    const iconName = iconMap.fileExtensions[ext] || langIcon;
     if (
-      !overrideMap.deletions[`extensions:${lang.id}`] &&
       !iconMap.fileExtensions[ext] &&
       iconName && iconMap.iconDefinitions[iconName]
     ) {
@@ -160,10 +153,10 @@ function mapLanguageContribution(lang) {
     }
   });
   lang.filenames.forEach((name) => {
-    const iconName = handleIconRemapping(lang.id, 'filenames', 'fileNames', name);
+    const iconName = iconMap.fileNames[name] || langIcon;
     if (
-      !overrideMap.deletions[`filenames:${lang.id}`] &&
       !iconMap.fileNames[name] &&
+      !(name.startsWith('.') && iconMap.fileExtensions[name.substring(1)]) &&
       iconName && iconMap.iconDefinitions[iconName]
     ) {
       languageMap.fileNames[name] = iconName;
@@ -176,31 +169,12 @@ function mapLanguageContribution(lang) {
   }
 }
 
-function generateLanguageMap() {
+async function generateLanguageMap() {
   console.log('[6/7] Writing language contribution map to icon configuration file.');
   fs.writeFileSync(
     path.resolve(srcPath, 'language-map.json'),
     stringify(languageMap, { space: '  ' })
   );
   console.log('[7/7] Deleting language contribution cache.');
-  rimraf.sync(vsDataPath);
-}
-
-function handleIconRemapping(language, overrideType, contributionType, value) {
-  const override = overrideMap[overrideType][language];
-  if (typeof override === 'object') {
-    if (override[value]) {
-      return override[value];
-    }
-    const matchedPattern = Object.keys(override)
-      .filter(p => p.charAt(0) === '^')
-      .find(p => value.startsWith(p.substring(1)));
-    if (matchedPattern) {
-      return override[matchedPattern];
-    }
-  }
-  if (typeof override === 'string') {
-    return override;
-  }
-  return iconMap[contributionType][value] || iconMap.languageIds[language];
+  await fs.remove(vsDataPath);
 }
