b66bf2053c1b93f269f2251540d3abb350b57826
Chris Sandvik <chris.sandvik@gmail.com>
2023-02-27 18:00:25 -0500

    Add support for the new GitHub code view

--- src/providers/github.js
+++ src/providers/github.js
@@ -1,26 +1,51 @@
+const HIDE_ELEMENT_CLASS = 'github-material-icons-exension-hide';
+
 const githubConfig = {
   domain: 'github.com',
   selectors: {
-    row: '.js-navigation-container[role=grid] > .js-navigation-item, file-tree .ActionList-content, a.tree-browser-result',
-    filename:
-      'div[role="rowheader"] > span, .ActionList-item-label, a.tree-browser-result > marked-text',
-    icon: '.octicon-file, .octicon-file-directory-fill, .octicon-file-submodule, a.tree-browser-result > svg.octicon.octicon-file',
+    row: `.js-navigation-container[role=grid] > .js-navigation-item,
+      file-tree .ActionList-content,
+      a.tree-browser-result,
+      .PRIVATE_TreeView-item-content,
+      .react-directory-filename-column`,
+    filename: `div[role="rowheader"] > span,
+      .ActionList-item-label,
+      a.tree-browser-result > marked-text,
+      .PRIVATE_TreeView-item-content > .PRIVATE_TreeView-item-content-text,
+      .react-directory-filename-column h3`,
+    icon: `.octicon-file,
+      .octicon-file-directory-fill,
+      .octicon-file-directory-open-fill,
+      .octicon-file-submodule,
+      .react-directory-filename-column > svg`,
   },
   getIsLightTheme: () => document.querySelector('html').getAttribute('data-color-mode') === 'light',
-  getIsDirectory: ({ icon }) => icon.getAttribute('aria-label') === 'Directory',
+  getIsDirectory: ({ icon }) =>
+    icon.getAttribute('aria-label') === 'Directory' ||
+    icon.classList.contains('octicon-file-directory-fill') ||
+    icon.classList.contains('octicon-file-directory-open-fill') ||
+    icon.classList.contains('icon-directory'),
   getIsSubmodule: ({ icon }) => icon.getAttribute('aria-label') === 'Submodule',
   getIsSymlink: ({ icon }) => icon.getAttribute('aria-label') === 'Symlink Directory',
   replaceIcon: (svgEl, newSVG) => {
-    svgEl
-      .getAttributeNames()
-      .forEach(
-        (attr) =>
-          attr !== 'src' &&
-          !/^data-material-icons-extension/.test(attr) &&
-          newSVG.setAttribute(attr, svgEl.getAttribute(attr))
-      );
+    newSVG.style.height = '1rem';
+    newSVG.style.width = '1rem';
+    newSVG.style.verticalAlign = 'text-bottom';
+    newSVG.style.userSelect = 'none';
 
-    svgEl.parentNode.replaceChild(newSVG, svgEl);
+    if (!svgEl.classList.contains(HIDE_ELEMENT_CLASS)) {
+      svgEl.classList.add(HIDE_ELEMENT_CLASS);
+    }
+
+    // Instead of replacing the icon, add the new icon as a previous sibling,
+    // otherwise the GitHub code view crashes when you navigate through the
+    // tree view
+    const prevEl = svgEl.previousElementSibling;
+    if (prevEl?.getAttribute('data-material-icons-extension') === 'icon') {
+      svgEl.parentNode.replaceChild(newSVG, prevEl);
+    } else {
+      svgEl.parentNode.insertBefore(newSVG, svgEl);
+    }
   },
   onAdd: () => {},
 };
