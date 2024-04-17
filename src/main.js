267a74189002248bad3ee0ac708147de55289217
Michael Goodman <bulgedition@gmail.com>
2024-04-18 01:38:31 +0300

    added loading of custom providers

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
-import { getGitProvider } from './providers';
+import { getGitProvider, addGitProvider } from './providers';
 import { observePage, replaceAllIcons } from './lib/replace-icons';
 import { initIconSizes } from './lib/icon-sizes';
 import { getConfig, onConfigChange } from './lib/userConfig';
+import { getCustomProviders } from './lib/custom-providers';
 
-initIconSizes();
-const { href } = window.location;
-const gitProvider = getGitProvider(href);
+async function loadCustomProviders() {
+  const customProviders = await getCustomProviders();
 
-Promise.all([
-  getConfig('iconPack'),
-  getConfig('extEnabled'),
-  getConfig('extEnabled', 'default'),
-]).then(([iconPack, extEnabled, globalExtEnabled]) => {
-  if (!globalExtEnabled || !extEnabled || !gitProvider) return;
-  observePage(gitProvider, iconPack);
-  onConfigChange('iconPack', (newIconPack) => replaceAllIcons(gitProvider, newIconPack));
-});
+  for (const [domain, handler] of Object.entries(customProviders)) {
+    addGitProvider(domain, handler);
+  }
+}
+
+async function init() {
+  initIconSizes();
+
+  await loadCustomProviders();
+
+  const { href } = window.location;
+  const gitProvider = getGitProvider(href);
+
+  Promise.all([
+    getConfig('iconPack'),
+    getConfig('extEnabled'),
+    getConfig('extEnabled', 'default'),
+  ]).then(([iconPack, extEnabled, globalExtEnabled]) => {
+    if (!globalExtEnabled || !extEnabled || !gitProvider) return;
+    observePage(gitProvider, iconPack);
+    onConfigChange('iconPack', (newIconPack) => replaceAllIcons(gitProvider, newIconPack));
+  });
+}
+
+init();
