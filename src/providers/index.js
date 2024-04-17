eceb63ba6fbd9ac29ceecab5eacaaf73cb67af7a
Michael Goodman <bulgedition@gmail.com>
2024-04-18 01:35:57 +0300

    getGitProvider now tests only the host of the url instead of the whole string. Removed switch statement for thesting domains in favor of a for loop which will allow custom domains. added new function addGitProvider which adds a copy of the config for given provider and new custom domain

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,20 +1,28 @@
-import githubConfig from './github';
-import bitbucketConfig from './bitbucket';
-import azureConfig from './azure';
-import giteaConfig from './gitea';
-import gitlabConfig from './gitlab';
-import giteeConfig from './gitee';
-import sourceforgeConfig from './sourceforge';
+import github from './github';
+import bitbucket from './bitbucket';
+import azure from './azure';
+import gitea from './gitea';
+import gitlab from './gitlab';
+import gitee from './gitee';
+import sourceforge from './sourceforge';
 
-export const providerConfig = {
-  github: githubConfig,
-  bitbucket: bitbucketConfig,
-  azure: azureConfig,
-  gitea: giteaConfig,
-  gitlab: gitlabConfig,
-  gitee: giteeConfig,
-  sourceforge: sourceforgeConfig,
+export const providers = {
+  azure,
+  bitbucket,
+  gitea,
+  gitee,
+  github,
+  gitlab,
+  sourceforge,
 };
+
+export const providerConfig = {};
+
+for (const provider of Object.values(providers)) {
+  const cfg = provider();
+
+  providerConfig[cfg.name] = cfg;
+}
 
 /**
  * Get all selectors and functions specific to the Git provider
@@ -23,30 +31,29 @@ export const providerConfig = {
  * @returns {object} All of the values needed for the provider
  */
 export const getGitProvider = (href) => {
-  switch (true) {
-    case /github\.com.*/.test(href):
-      return providerConfig.github;
-
-    case /bitbucket\.org/.test(href):
-      return providerConfig.bitbucket;
-
-    case /gitea\.com/.test(href):
-      return providerConfig.gitea;
-
-    case /dev\.azure\.com/.test(href):
-    case /visualstudio\.com/.test(href):
-      return providerConfig.azure;
-
-    case /gitlab\.com/.test(href):
-      return providerConfig.gitlab;
-
-    case /gitee\.com/.test(href):
-      return providerConfig.gitee;
-
-    case /sourceforge\.net/.test(href):
-      return providerConfig.sourceforge;
-
-    default:
-      return null;
+  if (!href.startsWith('http')) {
+    href = new URL(`http://${href}`);
   }
+
+  for (const provider of Object.values(providerConfig)) {
+    for (const domain of provider.domains) {
+      if (domain.test(href.host)) {
+        return provider;
+      }
+    }
+  }
+
+  return null;
+};
+
+/**
+ * Add custom git provider
+ *
+ * @param {string} name
+ * @param {string|CallableFunction} handler
+ */
+export const addGitProvider = (name, handler) => {
+  handler = typeof handler === 'string' ? providers[handler] : handler;
+
+  providerConfig[name] = handler();
 };
