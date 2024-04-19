25c5c2a6345769df05b60b95744819f63aa0b934
Michael Goodman <bulgedition@gmail.com>
2024-04-20 02:39:14 +0300

    wip: custom domains

--- src/providers/index.js
+++ src/providers/index.js
@@ -5,6 +5,7 @@ import gitea from './gitea';
 import gitlab from './gitlab';
 import gitee from './gitee';
 import sourceforge from './sourceforge';
+import { getCustomProviders } from '../lib/custom-providers';
 
 export const providers = {
   azure,
@@ -24,27 +25,9 @@ for (const provider of Object.values(providers)) {
   providerConfig[cfg.name] = cfg;
 }
 
-/**
- * Get all selectors and functions specific to the Git provider
- *
- * @param {string} href Url of current tab
- * @returns {object} All of the values needed for the provider
- */
-export const getGitProvider = (href) => {
-  if (!href.startsWith('http')) {
-    href = new URL(`http://${href}`);
-  }
-
-  for (const provider of Object.values(providerConfig)) {
-    for (const domain of provider.domains) {
-      if (domain.test(href.host)) {
-        return provider;
-      }
-    }
-  }
-
-  return null;
-};
+function regExpEscape(str) {
+  return str.replace(/[-[\]{}()*+!<=:?./\\^$|#\s,]/g, '\\$&');
+}
 
 /**
  * Add custom git provider
@@ -55,6 +38,53 @@ export const getGitProvider = (href) => {
 export const addGitProvider = (name, handler) => {
   handler = typeof handler === 'string' ? providers[handler] : handler;
 
-  providerConfig[name] = handler();
-  providerConfig[name].isCustom = true;
+  const provider = handler();
+  provider.isCustom = true;
+  provider.name = name;
+  provider.domains = [
+    {
+      host: name,
+      test: new RegExp(`^${regExpEscape(name)}$`),
+    },
+  ];
+
+  providerConfig[name] = provider;
+};
+
+export const getGitProviders = () =>
+  getCustomProviders().then((customProviders) => {
+    for (const [domain, handler] of Object.entries(customProviders)) {
+      if (!providerConfig[domain]) {
+        addGitProvider(domain, handler);
+      }
+    }
+
+    return providerConfig;
+  });
+
+/**
+ * Get all selectors and functions specific to the Git provider
+ *
+ * @param {string} href Url of current tab
+ * @param domain
+ * @returns {object} All of the values needed for the provider
+ */
+export const getGitProvider = (domain) => {
+  if (!domain.startsWith('http')) {
+    domain = new URL(`http://${domain}`).host;
+  } else {
+    domain = new URL(domain).host;
+  }
+
+  return getGitProviders().then((p) => {
+    for (const provider of Object.values(p)) {
+      for (const d of provider.domains) {
+        if (d.test.test(domain)) {
+          return provider;
+        }
+      }
+    }
+
+    return null;
+  });
 };
