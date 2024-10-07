85d58330dd3db7ce317e210e7993e68f4f3ca3de
Philipp Kief <philipp.kief@gmx.de>
2024-10-07 23:02:47 +0200

    Delete unused code

--- src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-/**
- * A helper function to check if an object has a key value, without including prooerties from the prototype chain.
- */
-export function objectHas(obj: object, key: string) {
-  return obj.hasOwnProperty(key);
-}
