522682caf4a8c35e223282f96e7baa80f85e6d6c
Philipp Kief <philipp.kief@gmx.de>
2024-10-14 19:49:30 +0200

    Clean up code

--- src/ui/options/components/icon-settings-dialog.tsx
+++ src/ui/options/components/icon-settings-dialog.tsx
@@ -5,11 +5,8 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
-  Slide,
   Typography,
 } from '@mui/material';
-import { TransitionProps } from '@mui/material/transitions';
-import { ReactElement, Ref, forwardRef } from 'react';
 import { FileIconBindings } from './file-icon-bindings';
 import { FolderIconBindings } from './folder-icon-bindings';
 import { LanguageIconBindings } from './language-icon-bindings';
@@ -20,28 +17,13 @@ type IconSettingsDialogProps = {
   onClose: () => void;
 };
 
-const Transition = forwardRef(function Transition(
-  props: TransitionProps & {
-    children: ReactElement<unknown>;
-  },
-  ref: Ref<unknown>
-) {
-  return <Slide direction='up' ref={ref} {...props} />;
-});
-
 export function IconSettingsDialog({
   show,
   domain,
   onClose,
 }: IconSettingsDialogProps) {
   return (
-    <Dialog
-      open={show}
-      onClose={onClose}
-      TransitionComponent={Transition}
-      fullWidth={true}
-      maxWidth='lg'
-    >
+    <Dialog open={show} onClose={onClose} fullWidth={true} maxWidth='lg'>
       <DialogTitle>Configure Icon Bindings</DialogTitle>
       <DialogContent>
         <Typography component='div' style={{ paddingBottom: '1.5rem' }}>
