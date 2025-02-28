9594088b0af59d915817061efd5332ecb7f2da2d
Philipp Kief <philipp.kief@gmx.de>
2025-02-28 17:50:11 +0100

    Add descriptive comment for the mutation observer

--- src/lib/icon-sizes.ts
+++ src/lib/icon-sizes.ts
@@ -8,9 +8,13 @@ const ATTRIBUTE_NAME = 'data-material-icons-extension-size';
 const setSizeAttribute = (iconSize: IconSize) =>
   document.body.setAttribute(ATTRIBUTE_NAME, iconSize);
 
+/**
+ * The mutation observer ensures that the body tag will have the correct attribute
+ * all the time because some websites (e.g. GitHub) remove it while navigating.
+ */
 const observeBodyChanges = () => {
   const observer = new MutationObserver(() => {
-    getConfig('iconSize').then(iconSize => {
+    getConfig('iconSize').then((iconSize) => {
       if (!document.body.hasAttribute(ATTRIBUTE_NAME)) {
         setSizeAttribute(iconSize);
       }
@@ -23,10 +27,14 @@ const observeBodyChanges = () => {
 export const initIconSizes = () => {
   const setIconSize = () => getConfig('iconSize').then(setSizeAttribute);
 
-  document.addEventListener('DOMContentLoaded', () => {
-    setIconSize();
-    observeBodyChanges();
-  }, false);
+  document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+      setIconSize();
+      observeBodyChanges();
+    },
+    false
+  );
 
   addConfigChangeListener('iconSize', setSizeAttribute);
   addConfigChangeListener('iconSize', setIconSize, 'default');
